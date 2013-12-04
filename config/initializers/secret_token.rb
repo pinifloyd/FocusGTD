93bd1740228cbe0a7be212200b3e646d8dbf4980
Sergey Hanchar <hanchar.sergey@gmail.com>
2013-12-04 22:50:52 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FocusGTD::Application.config.secret_key_base = '9619bc6699e738e9f775b22d9848402d6e1b0c3f8fdf0bcb8869c22969f298d016317d865462a00dc59d51031dfe8d743fb60324004058c956886b53f6762075'
