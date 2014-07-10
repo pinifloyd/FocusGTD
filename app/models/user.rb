617c941b2cb9deb8b214a35bc3b128b7e56827f2
Sergey Hanchar <hanchar.sergey@gmail.com>
2014-07-10 23:28:37 +0300

    Annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  email              :string(255)      default(""), not null
+#  encrypted_password :string(255)      default(""), not null
+#  created_at         :datetime
+#  updated_at         :datetime
+#
+
 class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :validatable
